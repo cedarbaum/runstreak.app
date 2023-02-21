9ff70b0b44b13e3926cb95168adaadcf5de853ab
Samuel Cedarbaum <scedarbaum@gmail.com>
2023-02-21 18:50:09 -0500

    Make sign-in element a button instead of anchor

--- components/Header.tsx
+++ components/Header.tsx
@@ -57,9 +57,10 @@ export default function Header({ athlete }: HeaderProps) {
         {sessionStatus !== "loading" && (
           <div className="items-center justify-end md:flex md:flex-1 lg:w-0">
             {!session ? (
-              <a
+              <button
                 className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
                 onClick={() => signIn("strava")}
+                type="button"
               >
                 <Image
                   src="btn_strava_connectwith_orange.svg"
@@ -67,7 +68,7 @@ export default function Header({ athlete }: HeaderProps) {
                   height={48}
                   alt={"Connect with Strava"}
                 />
-              </a>
+              </button>
             ) : (
               <SettingsPopover />
             )}
