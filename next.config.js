7257c0c150e99e662afd4cf7002a206773fc9d7d
Samuel Cedarbaum <scedarbaum@gmail.com>
2023-03-09 15:22:01 -0500

    Explicitly disable cache on API routes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-}
+  headers: () => [
+    {
+      source: "/api/(.*)",
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "no-store",
+        },
+      ],
+    },
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
