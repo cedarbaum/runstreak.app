eefd7644f773d47ab9b9640e58c2e3fbff08aedc
Samuel Cedarbaum <scedarbaum@gmail.com>
2023-03-09 15:34:57 -0500

    Fix last commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,13 +10,15 @@ import Layout from "@/components/layout";
 import { Analytics } from "@vercel/analytics/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const twentyFourHoursInMs = 1000 * 60 * 60 * 24;
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
-      refetchOnmount: false,
+      refetchOnMount: false,
       refetchOnReconnect: false,
       retry: false,
+      staleTime: twentyFourHoursInMs,
     },
   },
 });
