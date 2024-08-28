1ebc1118869a46fdd4646be5180eb9716729401a
Samuel Cedarbaum <scedarbaum@gmail.com>
2024-08-28 02:31:47 -0400

    Use built-in prefix feature of rate limiting library

--- utils/RateLimiting.ts
+++ utils/RateLimiting.ts
@@ -2,12 +2,14 @@ import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 import crypto from "crypto";
 
-const APP_NAME = "runstreak.app";
+const RATE_LIMIT_PREFIX = "runstreak.app";
 
 const requestLimit = parseInt(process.env.STRAVA_DAILY_API_LIMIT!);
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
   limiter: Ratelimit.fixedWindow(requestLimit, "24 h"),
+  prefix: `${RATE_LIMIT_PREFIX}-request`,
+  analytics: false,
 });
 
 export default async function stravaUserCanMakeRequest(
@@ -17,7 +19,6 @@ export default async function stravaUserCanMakeRequest(
     .createHash("sha256")
     .update(accountId.toString())
     .digest("hex");
-  const id = `${APP_NAME}_${encryptedAccountId}`;
-  const { success } = await ratelimit.limit(id);
+  const { success } = await ratelimit.limit(encryptedAccountId);
   return success;
 }
